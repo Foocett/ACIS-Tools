dbab9c3abb3deaa7e8ba005b2908a0cf98b3095c
Andrew Fawcett <ajfawcett@gmail.com>
2025-06-26 15:49:45 -0500

    log modifications

--- simTest.py
+++ simTest.py
@@ -1,6 +1,6 @@
 import AcisCanTools as ACT
 
 log = ACT.Parser(
-    mode="logged", interface=ACT.utils.get_can_interface(), output_name="simTest.log")
+    mode="logged", interface=ACT.utils.get_can_interface(), output_name="simTest.log", loopback=True)
 log.configure_smart_nox_output()
 log.beginLogging()
